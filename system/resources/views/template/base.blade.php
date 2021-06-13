13cc0d806953908dcd6ed4dbe773e77ea82ecf76
Maulida97 <maulidanandar97@gmail.com>
2021-06-13 17:26:13 +0700

    Update base.blade.php

--- system/resources/views/template/base.blade.php
+++ system/resources/views/template/base.blade.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -33,17 +29,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   </div>
   <!-- /.content-wrapper -->
 
-  {{-- <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar --> --}}
 
-  <!-- Main Footer -->
   @include('template.section.footer')
 <!-- ./wrapper -->
 
