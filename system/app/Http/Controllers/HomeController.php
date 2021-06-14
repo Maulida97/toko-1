fe00387db1e887fa5e4b1a4c92ba5869db82c151
Maulida97 <maulidanandar97@gmail.com>
2021-06-14 11:47:58 +0700

    Update HomeController.php

--- system/app/Http/Controllers/HomeController.php
+++ system/app/Http/Controllers/HomeController.php
@@ -5,21 +5,8 @@ namespace App\Http\Controllers;
 class HomeController extends Controller
 {
 
-    function showBeranda()
-    {
-        return view('beranda');
-    }
-
-    function showProduk()
-    {
-        return view('produk');
-    }
-    function showPesanan()
-    {
-        return view('pesanan');
-    }
-    function showUser()
-    {
-        return view('user');
-    }
+    // function showBeranda()
+    // {
+    //     return view('beranda');
+    // }
 }
