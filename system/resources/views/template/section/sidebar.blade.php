debb41c137690dcb32cda1b55adc3f274ae7f529
Maulida97 <maulidanandar97@gmail.com>
2021-06-14 23:03:31 +0700

    Update sidebar.blade.php

--- system/resources/views/template/section/sidebar.blade.php
+++ system/resources/views/template/section/sidebar.blade.php
@@ -7,9 +7,9 @@
 
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{url('beranda')}}" class="brand-link">
+    <a href="{{url('toko')}}" class="brand-link">
       <img src="{{url('public')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Zhopee</span>
+      <span class="brand-text font-weight-light">Shopii</span>
     </a>
 
     <!-- Sidebar -->
